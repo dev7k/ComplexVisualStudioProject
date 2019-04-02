83ca6dc4f245d09e5e7500d216f0e3c79c744528
dev7k <dev7k@protonmail.com>
2019-04-02 19:49:50 +0200

    added projects net46

---
+++ src/App.InfrastructureLayer/Common/StaticLogger.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+
+namespace App.InfrastructureLayer.Common
+{
+    public static class StaticLogger
+    {
+        public static void LogInfo(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Info(text);
+        }
+
+        public static void LogWarn(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Warn(text);
+        }
+
+        public static void LogDebug(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Debug(text);
+        }
+
+        public static void LogTrace(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Trace(text);
+        }
+
+        public static void LogError(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Error(text);
+        }
+
+        public static void LogFatal(Type type, string text)
+        {
+            LogManager.GetLogger(type.FullName).Fatal(text);
+        }
+    }
+}
---
+++ src/App.InfrastructureLayer/Dto/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿namespace App.InfrastructureLayer.Dto
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public EmployeeDto() : this("John", "Wayne", 55)
+        {
+        }
+
+        public EmployeeDto(string firstname, string lastName, int age)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
