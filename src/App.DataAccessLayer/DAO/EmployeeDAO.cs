9a36497ddd5b7057fca803ddfbedbfada9b597c4
dev7k <dev7k@protonmail.com>
2019-04-02 21:41:22 +0200

    added employee dao class

---
+++ src/App.DataAccessLayer/DAO/EmployeeDAO.cs
@@ -0,0 +1,67 @@
+﻿using App.DataAccessLayer.Models;
+using App.InfrastructureLayer.Common;
+using App.InfrastructureLayer.Dto;
+using System;
+using System.Linq;
+
+namespace App.DataAccessLayer.DAO
+{
+    public class EmployeeDAO : ApplicationBase
+    {
+        public EmployeeDAO() : base(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+        {
+        }
+
+        public EmployeeDto CreateEmployee(EmployeeDto employeeDto)
+        {
+            try
+            {
+                using (var database = new EmployeeTrainingModelContainer())
+                {
+                    var employee = new Employee
+                    {
+                        EmployeeNumber = employeeDto.EmployeeNumber,
+                        FirstName = employeeDto.FirstName,
+                        LastName = employeeDto.LastName,
+                        DateOfBirth = employeeDto.DateOfBirth,
+                        DateOfHire = employeeDto.DateOfHire
+                    };
+
+                    database.EmployeeSet.Add(employee);
+                    database.SaveChanges();
+
+                    var query = from e in database.EmployeeSet
+                                where e.EmployeeNumber == employeeDto.EmployeeNumber
+                                select e;
+
+                    employeeDto.Id = query.First().Id;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+
+            return employeeDto;
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            try
+            {
+                using (var database = new EmployeeTrainingModelContainer())
+                {
+                    Employee employee = (Employee)database.EmployeeSet.Where(e => e.Id == id).First();
+
+                    database.EmployeeSet.Remove(employee);
+                    database.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+    }
+}
